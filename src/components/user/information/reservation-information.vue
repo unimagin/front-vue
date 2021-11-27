1c4f3c791be11eef48c81e2d8528b44e504127c5
0whatever <840065505@qq.com>
2021-11-27 18:27:58 +0800

    beautify bills_show

--- src/components/user/information/reservation-information.vue
+++ src/components/user/information/reservation-information.vue
@@ -1,72 +1,104 @@
 <template>
-  <el-table :data="bills" style="width: 100%;margin-left: -80px;margin-top: -10px">
-    <el-table-column type="expand">
-      <template v-slot="scope">
-        <el-descriptions
+  <div
+    class="main-content"
+    style="width: 100%; margin-left: -80px; margin-top: -10px"
+  >
+    <el-table :data="bills" :row-style="rowStyle">
+      <el-table-column type="expand">
+        <template v-slot="scope">
+          <el-descriptions
             title="详细信息"
             direction="vertical"
             :column="3"
             border
-        >
-          <el-descriptions-item label="预约情况" align="center">{{
-              showStatus(scope.row.status)
-            }}
-          </el-descriptions-item>
-          <el-descriptions-item label="到达时间" align="center">{{
-              date(scope.row.arrive_time)
-            }}
-          </el-descriptions-item>
-          <el-descriptions-item label="离开时间 " align="center">{{
-              date(scope.row.leave_time)
-            }}
-          </el-descriptions-item>
-        </el-descriptions>
-      </template>
-    </el-table-column>
-    <el-table-column type="index"/>
-    <el-table-column label="预约车辆" prop="car_number" width="150">
-    </el-table-column>
-    <el-table-column label="开始时间" width="100">
-      <template v-slot="scope">
-        {{ date(scope.row.begin_time) }}
-      </template>
-    </el-table-column>
-    <el-table-column label="结束时间" width="100">
-      <template v-slot="scope">
-        {{ date(scope.row.end_time) }}
-      </template>
-    </el-table-column>
-    <el-table-column label="费用" prop="money"></el-table-column>
-    <el-table-column label="操作" width="150">
-      <template v-slot="scope">
-        <el-button
+          >
+            <el-descriptions-item label="预约情况" align="center"
+              >{{ showStatus(scope.row.status) }}
+            </el-descriptions-item>
+            <el-descriptions-item label="到达时间" align="center"
+              >{{ date(scope.row.arrive_time) }}
+            </el-descriptions-item>
+            <el-descriptions-item label="离开时间 " align="center"
+              >{{ date(scope.row.leave_time) }}
+            </el-descriptions-item>
+          </el-descriptions>
+        </template>
+      </el-table-column>
+      <el-table-column type="index">
+        <template v-slot="scope">
+          <span>{{ this.current_page * 15 + 1 + scope.$index }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="预约车辆" prop="car_number" width="150">
+      </el-table-column>
+      <el-table-column label="开始时间" width="100">
+        <template v-slot="scope">
+          {{ date(scope.row.begin_time) }}
+        </template>
+      </el-table-column>
+      <el-table-column label="结束时间" width="100">
+        <template v-slot="scope">
+          {{ date(scope.row.end_time) }}
+        </template>
+      </el-table-column>
+      <el-table-column label="费用" prop="money"></el-table-column>
+      <el-table-column label="操作" width="150">
+        <template v-slot="scope">
+          <el-button
             size="primary"
             round
             @click="pay(scope.row)"
             v-show="scope.row.status != 2"
-        >{{ scope.row.isPaid ? "已支付" : "待支付" }}
-        </el-button
-        >
-      </template>
-    </el-table-column>
-  </el-table>
+            >{{ scope.row.isPaid ? "已支付" : "待支付" }}
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @current-change="currentChange"
+      :page-size="15"
+      layout="prev, pager, next"
+      :total="this.sum"
+      style="margin-top: 30px"
+      center
+    >
+    </el-pagination>
+  </div>
 </template>
 <script lang="ts">
 import axios from "axios";
-import {ElMessage} from "element-plus";
+import { ElMessage } from "element-plus";
 
 export default {
   data() {
     return {
       bills: [],
       seeDetail: false,
+      total_bills: [],
+      current_page: 0,
+      sum: 0,
     };
   },
   methods: {
+    rowStyle({ row, rowIndex }) {
+      console.log(rowIndex);
+      if (row.status == 2) {
+        return " background-color: #f4f4f5;";
+      } else if (row.status == 0) {
+        return " background-color: #e1f3d8";
+      } else if ((row.status = 4)) {
+        return " background-color: #fde2e2";
+      }
+      return "background-color: #fef0f0";
+    },
+    currentChange(size) {
+      this.current_page = size - 1;
+      this.bills = this.total_bills[this.current_page];
+    },
     date(time) {
       const timestr = new Date(time).toLocaleString().split(" ");
       return (
-          timestr[0] + "\n" + timestr[1].substring(0, timestr[1].lastIndexOf(":"))
+        timestr[0] + "\n" + timestr[1].substring(0, timestr[1].lastIndexOf(":"))
       );
     },
     showStatus(status) {
@@ -87,30 +119,34 @@ export default {
       if (row.isPaid == 0) {
         row.isPaid = 1;
         axios
-            .post("/api/user/bill/pay_bill", {bill_ID: row.bill_ID})
-            .then((resp) => {
-              ElMessage({
-                type: "success",
-                message: "支付成功！",
-              });
-            })
-            .catch((err) => {
-              console.log(err);
+          .post("/api/user/bill/pay_bill", { bill_ID: row.bill_ID })
+          .then((resp) => {
+            ElMessage({
+              type: "success",
+              message: "支付成功！",
             });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       }
     },
   },
   created() {
     const user = JSON.parse(localStorage.getItem("user"));
     axios
-        .post("/api/user/finished_reservation", user)
-        .then((resp) => {
-          this.bills = resp.data.bills;
-          console.log(this.bills);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      .post("/api/user/finished_reservation", user)
+      .then((resp) => {
+        this.sum = resp.data.bills.length;
+        const page = Math.ceil(this.sum / 15);
+        for (let i = 0; i < page; i++) {
+          this.total_bills[i] = resp.data.bills.slice(i * 15, (i + 1) * 15);
+        }
+        this.bills = this.total_bills[0];
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
 };
 </script>
